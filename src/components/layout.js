21ce5ec582b9c036281ca23fc4df1634bfbc00a0
Danny Burton <dev@dannyburton.ca>
2019-07-20 17:39:22 -0700

    Under construction page built

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import Particles from './particles';
-import Metadata from '../hooks/use-sitemetadata';
 import '../style/main.scss';
 
 const Layout = ({ children }) => {
-   const { title, description } = Metadata();
    return (
       <>
          <Helmet>
             <html lang="en" />
-            <title>{title}</title>
+            <title>Danny Burton | Web Developer</title>
+            <meta
+               name="description"
+               content="Portfolio for Vancouver-based web developer Danny Burton"
+            />
             <link rel="stylesheet" href="https://use.typekit.net/qjk7wuk.css" />
          </Helmet>
          <main>{children}</main>
