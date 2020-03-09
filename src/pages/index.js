240d074358f00fe255d35ffe4ab08ce51715291c
Danny Burton <wheredannyends@gmail.com>
2020-03-09 17:19:35 +0000

    Better splash

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,20 @@
 import React from 'react';
+import Logo from '../components/logo';
 
 const App = () => {
-   return (
-      <div className="intro">
-         <h2 className="intro__title">
-            Hi. My name is Danny. <br />
-            I'm a web developer
-         </h2>
-         <p className="intro__blurb">
-            I design and develop experiences on the internet.
-            <br />
-            I'm currently a <strong>Senior Frontend Developer</strong> at{' '}
-            <a href="https://www.convertus.com">Convertus</a>.
-         </p>
-         <p className="intro__disclaimer">
-            My new portfolio isn't quite ready. For now, please check out my{' '}
-            <a href="http://www.dannyburton.ca">old one</a>.
-         </p>
-         <svg
-            className="logo"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 420 420"
-         >
-            <path d="M210,0C94,0,0,94,0,210a210.82,210.82,0,0,0,4.57,43.7l9.11-5.5A200.19,200.19,0,0,1,10,210C10,99.72,99.72,10,210,10S410,99.72,410,210a200.68,200.68,0,0,1-2.38,30.86l9.23,5.57A211.28,211.28,0,0,0,420,210C420,94,326,0,210,0Z" />
-            <path d="M95.7,241.52l37-62.58,22.9,19.77,27-23.33,27,23.33,27-23.33,27,23.33,24.13-20.83,49.6,83.92L390,230.21l8.34,5A190.88,190.88,0,0,0,400,210c0-104.77-85.23-190-190-190S20,105.23,20,210a190.42,190.42,0,0,0,2.82,32.68L60,220.21ZM163.91,126.4h93.34L284,171.48l-20.27,17.37-27-23.35-27,23.35-27-23.35-27,23.43-19-16.38Z" />
-            <path d="M390,241.88l-66.65,40.06-33.42-20.19-56.57,34.06-73.19-44.11-33.42,20.13-66.65-40L7,263.91C30.8,353.77,112.66,420,210,420c99.88,0,183.46-69.73,204.75-163.16Z" />
-         </svg>
-      </div>
-   );
+  return (
+    <div className="intro">
+      <Logo />
+      <h2>Hi. I&apos;m Danny Burton.</h2>
+      <h3>I&apos;m a Front-End Developer.</h3>
+      <ul>
+        <li>Currently a <strong>Frontend Developer</strong> at <a target="_blank" rel="noopener noreferrer" href="https://www.pixelunion.net">Pixel Union</a>.</li>
+        <li>Previously a <strong>Senior Frontend Developer</strong> at <a target="_blank" rel="noopener noreferrer" href="https://www.convertus.com">Convertus</a>.</li>
+        <li>I also <strong>freelance</strong> on the side. Check out <a target="_blank" rel="noopener noreferrer" href="https://www.ocin.co">OCIN</a> and <a target="_blank" rel="noopener noreferrer" href="https://www.rodengray.com">Roden Gray</a>.</li>
+        <li>Want to chat about a project? Feel free to <a target="_blank" rel="noopener noreferrer" href="mailto:iam@dannyburton.dev">send me an email</a>.</li>
+      </ul>
+    </div>
+  );
 };
 
 export default App;
