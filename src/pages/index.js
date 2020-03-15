256461802c8ce2e162b222d6d00710873798a215
Danny Burton <wheredannyends@gmail.com>
2020-03-15 14:37:49 +0000

    Update index

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,92 +3,92 @@ import { TimelineLite, Back, Power3 } from 'gsap';
 import TransitionLink from 'gatsby-plugin-transition-link';
 
 const App = () => {
-   let [homeRef, titleRef, bodyRef, buttonsRef] = useRef(null);
+  let [homeRef, titleRef, bodyRef, buttonsRef] = useRef(null);
 
-   useEffect(() => {
-      let TL = new TimelineLite();
+  useEffect(() => {
+    let TL = new TimelineLite();
 
-      const animation = {
-         settings: [
-            2,
-            {
-               opacity: 0,
-               y: 50,
-               scale: 0.9,
-               ease: Back.easeOut,
-            },
-         ],
-         delay: '-=1.8',
-      };
+    const animation = {
+      settings: [
+        2,
+        {
+          opacity: 0,
+          y: 50,
+          scale: 0.9,
+          ease: Back.easeOut,
+        },
+      ],
+      delay: '-=1.8',
+    };
 
-      TL.to(homeRef, 0, { css: { visibility: 'visible' } })
-         .from(titleRef, ...animation.settings)
-         .from(bodyRef, ...animation.settings, animation.delay)
-         .from(buttonsRef, ...animation.settings, animation.delay);
-      return () => {
-         TL.kill();
-      };
-   }, []);
+    TL.to(homeRef, 0, { css: { visibility: 'visible' } })
+      .from(titleRef, ...animation.settings)
+      .from(bodyRef, ...animation.settings, animation.delay)
+      .from(buttonsRef, ...animation.settings, animation.delay);
+    return () => {
+      TL.kill();
+    };
+  }, []);
 
-   const anim = {
-      exit: {
-         trigger: ({ node }) =>
-            TweenLite.to(node, 1.5, {
-               opacity: 0,
-               y: 50,
-               ease: Back.easeIn,
-            }),
-         length: 1.5,
-      },
-      entry: {
-         trigger: ({ node }) =>
-            TweenLite.from(node, 1.5, {
-               opacity: 0,
-            }),
-         delay: 1.5,
-      },
-   };
+  const anim = {
+    exit: {
+      trigger: ({ node }) =>
+        TweenLite.to(node, 1.5, {
+          opacity: 0,
+          y: 50,
+          ease: Back.easeIn,
+        }),
+      length: 1.5,
+    },
+    entry: {
+      trigger: ({ node }) =>
+        TweenLite.from(node, 1.5, {
+          opacity: 0,
+        }),
+      delay: 1.5,
+    },
+  };
 
-   return (
-      <section className="home" ref={el => (homeRef = el)}>
-         <h1 className="home__title" ref={el => (titleRef = el)}>
-            <span>Hi. My name is Danny Burton. </span>
-            <span>I'm a Front End Developer.</span>
-         </h1>
-         <p className="home__body" ref={el => (bodyRef = el)}>
-            <span>I develop user interfaces using web technologies. </span>
-            <span>
-               Currently a Senior Front-End Developer at{' '}
-               <a
-                  href="//www.convertus.com"
-                  target="_blank"
-                  className="home__body-link"
-               >
-                  Convertus
+  return (
+    <section className="home" ref={el => (homeRef = el)}>
+      <h1 className="home__title" ref={el => (titleRef = el)}>
+        <span>Hi. My name is Danny Burton. </span>
+        <span>I'm a Front End Developer.</span>
+      </h1>
+      <p className="home__body" ref={el => (bodyRef = el)}>
+        <span>I develop user interfaces using web technologies. </span>
+        <span>
+          Currently a Front End Developer at{' '}
+          <a
+            href="//www.pixelunion.net"
+            target="_blank"
+            className="home__body-link"
+          >
+            Pixel Union
                </a>
                .
             </span>
-         </p>
-         <div className="home__button-area" ref={el => (buttonsRef = el)}>
-            <TransitionLink
-               to="/contact"
-               exit={anim.exit}
-               entry={anim.entry}
-               className="home__button button button--alt"
-            >
-               Contact Me
+      </p>
+      <div className="home__button-area" ref={el => (buttonsRef = el)}>
+        <TransitionLink
+          to="/contact"
+          exit={anim.exit}
+          entry={anim.entry}
+          className="home__button button button--alt"
+        >
+          Contact Me
             </TransitionLink>
-            <TransitionLink
-               to="/projects"
-               exit={anim.exit}
-               entry={anim.entry}
-               className="home__button button"
-            >
-               View Projects
+        <TransitionLink
+          to="/projects"
+          exit={anim.exit}
+          entry={anim.entry}
+          className="home__button button"
+        >
+          View Projects
             </TransitionLink>
-         </div>
-      </section>
-   );
+      </div>
+    </section>
+  );
 };
 
 export default App;
